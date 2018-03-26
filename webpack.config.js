80f69b989081cb0985f846d00f2663294129c785
赵泰 <zhaotai.zt@alibaba-inc.com>
2018-03-26 14:18:13 +0800

    cannot work when installed because of webpack sources version

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack'); //访问内置的插件
+const EditableSourcesWebpackPlugin = require('./index');
+
+module.exports = {
+    mode: 'development',
+    entry: './test.js',
+    devtool: 'source-map',
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new EditableSourcesWebpackPlugin(/\.js$/, function (sourceCode) {
+            return `// ******** hello world! This is the embeded comment ********
+                ${sourceCode}
+            // ******** embeded comment end *******`;
+        })
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+  };
